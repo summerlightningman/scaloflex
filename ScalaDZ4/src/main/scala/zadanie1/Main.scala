6646e16215a23bb616e3f526322311628ae113ee
bagulnik <div8@bk.ru>
2019-11-14 01:06:11 +0300

    Исправления 4 Д/З

--- ScalaDZ4/src/main/scala/zadanie1/Main.scala
+++ ScalaDZ4/src/main/scala/zadanie1/Main.scala
@@ -2,22 +2,19 @@ package zadanie1
 
 object Main extends App {
 
+
 }
 
-class Stack(count: Int) {
-  private var arr = List[Int]()
+case class MultiList(size: Int) {
+  val head: Array[Int] = new Array[Int](size)
+  val next: Array[Int] = new Array[Int](size + 1)
+  val data: Array[Int] = new Array[Int](size + 1)
+  var count = 1
 
-  def push(num: Int): Unit = {
-    this.arr +:= num
-  }
-
-  def pop(): Int = {
-    val element = this.arr.last
-    this.arr.dropRight(1)
-    return element
-  }
-
-  def peek(): Int = {
-    return this.arr.last
+  def add(h: Int, v: Int): Unit = {
+    next(count) = head(h)
+    data(count) = v
+    count += 1
+    head(h) = count
   }
 }
--- ScalaDZ4/src/main/scala/zadanie2_4/Main.scala
+++ ScalaDZ4/src/main/scala/zadanie2_4/Main.scala
@@ -3,113 +3,124 @@ package zadanie2_4
 import java.sql.Timestamp
 
 object Main extends App {
+  def separate(): Unit = println("__________________________")
+
   val operations: List[BankOperation] = List[BankOperation](
-    BankOperation(Timestamp.valueOf("2019-05-05 13:33:50.234"), 0, 500, "input", 2),
-    BankOperation(Timestamp.valueOf("2019-05-05 14:33:50.234"), 1, 1500, "input", 0),
-    BankOperation(Timestamp.valueOf("2019-05-05 17:00:00.555"), 0, 300, "transfer", 1),
-    BankOperation(Timestamp.valueOf("2019-05-05 17:00:00.555"), 1, -300, "transfer", 0),
-    BankOperation(Timestamp.valueOf("2019-05-05 18:00:00.555"), 1, -1000, "food purchase", 3),
-    BankOperation(Timestamp.valueOf("2019-04-03 19:42:00.333"), 2, 340, "cinema", 5),
-    BankOperation(Timestamp.valueOf("2019-03-20 12:14:32.042"), 2, 100, "shaurma", 3),
-    BankOperation(Timestamp.valueOf("2020-04-03 19:42:00.333"), 1, 340, "cinema", 5),
-    BankOperation(Timestamp.valueOf("2020-04-05 04:20:00.000"), 2, 228, "blue kite", 1),
-    BankOperation(Timestamp.valueOf("2020-04-05 16:20:00.000"), 10, 199, "food purchase", 3),
-    BankOperation(Timestamp.valueOf("2020-04-06 08:43:02.020"), 1, 228, "food purchase", 3)
+    BankOperation(Timestamp.valueOf("2019-04-03 19:42:00.333"), 1, 340, "cinema", 5),
+    BankOperation(Timestamp.valueOf("2019-03-20 12:14:32.042"), 2, 100, "shaurma", 4),
+    BankOperation(Timestamp.valueOf("2019-03-20 12:14:32.042"), 2, 100, "shaurma", 6),
+    BankOperation(Timestamp.valueOf("2020-04-03 19:42:00.333"), 1, 340, "cinema", 4),
+    BankOperation(Timestamp.valueOf("2020-04-05 04:20:00.000"), 2, 228, "blue kite", 3),
+    BankOperation(Timestamp.valueOf("2020-04-05 16:20:00.000"), 1, 199, "food purchase", 3),
+    BankOperation(Timestamp.valueOf("2020-04-05 16:20:00.000"), 2, 199, "food purchase", 3),
+    BankOperation(Timestamp.valueOf("2020-04-05 16:20:00.000"), 1, 199, "food purchase", 4),
+    BankOperation(Timestamp.valueOf("2020-04-06 08:43:02.020"), 1, 228, "greetings", 5),
+    BankOperation(Timestamp.valueOf("2019-03-20 12:14:32.042"), 1, 1000, "donate", 2),
   )
 
   // 2.1 Остаток на счёте
   /* данные приводятся к виду:
-  сначала ((отправитель, получатель), деньги (общая сумма транзакций друг другу)),
-  далее (отправитель, -деньги), (получатель, деньги)
-  далее (отправитель, -деньги), (получатель, деньги)
-  далее, объединение данных, приведение к виду ИД -> Деньги
-  вывод
+      Получение отдельно транзакций отправителей с затратами (со знаком минус)
+      и получателей с прибылью
+      Далее, объединение списков отправителей и получателей
+      Их дальнейшая группировка, суммирование прибыли и затрат
+      Вывод в консоль
    */
-  operations.groupBy((operation: BankOperation) => (operation.userId, operation.source)).map {
-    case (users, operation) => (users, operation.map(operation => operation.operation).sum)
-  }.map {
-    case ((userId, source), operation) => List((userId, -operation), (source, operation))
-  }.flatten.groupBy((transaction) => transaction._1).map {
-    case (user, transaction) => (user, transaction.map {
-      case (_, operation) => operation
-    }.sum)
+  val receivers = operations.map(operation => (operation.userId, -operation.operation))
+  val senders = operations.map(operation => (operation.source, operation.operation))
+
+  val result = receivers ::: senders
+
+  result.groupBy(operation => operation._1).map {
+    case (userId, operation) => (userId, operation.map(operation => operation._2).sum)
   }.foreach {
-    case (user, money) => println(s"Остаток пользователя $user: $money у.е")
+    case (userId, balance) => println(s"User $userId has $balance units")
   }
+  separate()
 
   // 2.2 Наиболее частые операции по наименованию
   /*
-  Сначала группировка по наименованиям
-  Далее представление наименование - количество (было наименование - экземпляр класса операция)
-  Преобразование в список (для возможности сортировки), сортировка по 2 параметру (количеству),
-  обращение полученного списка в обратный порядок и получение первых 3 значений
-  Вывод на экран каждого оставшегося элемента
+      Группировка по наименованиям
+      Представление полученной мапы в виде ключ - количество
+      Преобразование в список для дальнейшей сортировки по значению
+      Реверс для получения сортировки в порядке убывания
+      Получение первых 3 значений
+      Вывод
   */
-  println("Наиболее частые операции по типу: ")
-  operations.groupBy((operation: BankOperation) => (operation.title)).map {
-    case (title, operation) => (title, operation.map((o: BankOperation) => o.title).length)
-  }.toList.sortBy(operation => operation._2).reverse.take(3)
-    .foreach { case (title, times) => println(s"$title - $times раз(а)") }
+  println("Most repeated operations: ")
+  operations.groupBy((operation) => (operation.title)).map {
+    case (title, operation) => (title, operation.length)
+  }.toList
+    .sortBy(operation => operation._2)
+    .reverse
+    .take(3)
+    .foreach {
+      case (title, times) => println(s"${title} in ${times} times")
+    }
+  separate()
 
 
   // 2.3 В какой части времени больше всего операций
   /*
-  Получение коллекции времён
-  Группировка по четверть дня - количество операций
-  Получение последнего элемента
+      Получение коллекции времён
+      Группировка по четверть дня - количество операций
+      Получение последнего элемента
   */
-  val operation = operations.map {
-    case (operation) => operation.eventDate
-  }.groupBy((day) => (day.toLocalDateTime.getHour / 4)).map {
-    case (quarter, day) => (quarter, day.length)
-  }.last
-  println(s"Наибольшее количество операций происходит в ${operation._1} четверти дня в количестве ${operation._2}")
+  def quarter(title: String = ""): Unit = {
+    val quad = operations.filter((operation) => operation.title == title).map {
+      case (operation) => operation.eventDate
+    }.groupBy((day) => (math.floor(day.toLocalDateTime.getHour / 6).toInt + 1)).map {
+      case (quarter, day) => (quarter, day.length)
+    }.maxBy(quarter => quarter._2)
+
+    println(s"The most count of operation ${title} does at ${quad._1} quarter of day in amount of ${quad._2} units")
+  }
+
+  quarter("cinema")
+  separate()
+
 
   // 3 Статистика по полам
   /*
-      Поиск суммы операций каждого пользователя
+      Группировка по ЮзерИД - имеют значения 1 и 2 в соответствии с полом
       Далее фильтр полученных значений по наличию их значения ИД в массиве
       Вывод
    */
-  val man = "Мужчин"
-  val woman = "Женщин"
+  val man = "man"
+  val woman = "woman"
   val userIdToGender: Map[Int, String] = Map[Int, String](1 -> man, 2 -> woman)
 
-  operations.groupBy((operation: BankOperation) => operation.userId).map {
-    case (userId, operation) => (userId, operation.map {
-      (operation) => operation.operation
-    }.sum)
-  }.filter {
-    (transaction) => userIdToGender.contains(transaction._1)
+  operations.groupBy(operation => operation.userId).map {
+    case (userId, operation) => (userId, operation.map(operation => operation.operation).sum)
   }.foreach {
-    case (userId, operation) =>
-      println(s"${userIdToGender(userId)}ы тратят $operation у.е")
+    case (userId, operation) => println(s"${userIdToGender(userId)}'s costs in ${operation} units")
   }
+  separate()
+
 
   // 4 Статистика
+  /*
+      Группировка сразу по наименованиям из списка
+      Представление в виде ключ: значение (через внутренню группировку по наименованиям, далее сумма всех расходов)
+      Для каждого полученного значения для каждого значения массива типов выводятся данные
+      также проверяется их наличие (наличие ключа)
+   */
   val types: Set[String] = Set("food purchase", "cinema")
 
-  operations.groupBy((operation) => operation.userId).map {
-    case (userId, operation) => (userId, operation.map(operation => (operation.title, operation.operation))
-      .filter(operation => types.contains(operation._1))
-      .groupBy { case (title, operation) => title }
-      .map {
-        case (title, operation) => (title, operation.map {
-          case (title, operation) => operation
-        }.sum)
-      })
-  }.foreach {
-    case (userId, operation) =>
-      val food: Double = if (operation.contains("food purchase")) operation("food purchase") else 0
-      val cinema: Double = if (operation.contains("cinema")) operation("cinema") else 0
-      println(s"Пользователь $userId потратил $food у.е. на еду и $cinema на поход в кино")
+  operations.filter(operation => types.contains(operation.title))
+    .groupBy(operation => operation.userId)
+    .map {
+      case (userId, operation) => (userId, operation
+        .map(operation => (operation.title, operation.operation))
+        .groupBy(operation => operation._1)
+        .map {
+          case (title, operation) => (title, operation.map(operation => operation._2).sum)
+        })
+    }.foreach {
+    case (userId, operations) =>
+      for (tpe <- types)
+        println(s"User's $userId costs for ${tpe} are equal ${if (!operations.contains(tpe)) 0 else operations(tpe)} units")
   }
-
-
 }
 
 case class BankOperation(eventDate: Timestamp, userId: Int, operation: Double, title: String, source: Int)
-
-
-
-
