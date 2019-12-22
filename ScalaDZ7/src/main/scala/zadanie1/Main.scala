c2bacce4cc7aac23067d7f2344a8c8ddb2350d82
bagulnik <div8@bk.ru>
2019-12-22 16:44:16 +0300

    Доделано 1 задание 6 Д/З

--- ScalaDZ7/src/main/scala/zadanie1/Main.scala
+++ ScalaDZ7/src/main/scala/zadanie1/Main.scala
@@ -1,8 +1,6 @@
 package main.scala.zadanie1
 
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.SparkSession
 
 // Задание: Finding 1st and 2nd Bestsellers Per Genre
 // Ссылка: http://blog.jaceklaskowski.pl/spark-workshop/exercises/spark-sql-exercise-Finding-1st-and-2nd-Bestsellers-Per-Genre.html
@@ -11,15 +9,8 @@ object Main extends App {
   val spark = SparkSession.builder().master("local[*]").appName("zadanie1").getOrCreate()
 
   val df = spark.read.format("csv").option("header", "true").load("books.csv")
-  val bestsellers = df.collect().groupBy(line => line(2))
-    .map {
-    case line => Row(line._2.sortBy(line => line.getString(3)).take(2))
-  }.toSeq
-  val cols = new StructType()
-    .add("id", StringType)
-    .add("title", StringType)
-    .add("genre", StringType)
-    .add("quantity", StringType)
-  val result = spark.createDataFrame(spark.sparkContext.parallelize(bestsellers), cols)
-  result.show()
+    .createOrReplaceTempView("book")
+  val sql = spark.sql("SELECT * FROM (SELECT id, title, genre, quantity, rank() OVER (PARTITION BY genre ORDER BY quantity DESC) as rank FROM book) WHERE rank <= 2")
+  .show()
+\
 }
