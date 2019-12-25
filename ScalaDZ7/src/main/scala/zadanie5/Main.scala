dde830421389968eca9277edbd0357d7b5dfbb07
bagulnik <div8@bk.ru>
2019-12-25 16:29:52 +0300

    Добавлен варианта 5 задания 7-го Д/З

--- ScalaDZ7/src/main/scala/zadanie5/Main.scala
+++ ScalaDZ7/src/main/scala/zadanie5/Main.scala
@@ -1,9 +1,9 @@
 package main.scala.zadanie5
 
 
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 // Задание: Finding Most Populated Cities Per Country
 // Ссылка: http://blog.jaceklaskowski.pl/spark-workshop/exercises/spark-sql-exercise-Finding-Most-Populated-Cities-Per-Country.html
@@ -11,5 +11,13 @@ import org.apache.spark.sql.functions._
 object Main extends App {
   val spark = SparkSession.builder().master("local[*]").appName("zadanie5").getOrCreate()
   val populations = spark.read.format("csv").option("header", "true").load("populations.csv")
-  //
+  val city = populations.groupBy("name").agg(min("population").as("c_population"))
+
+  populations
+    .groupBy("country")
+    .agg(max("population").as("population"))
+    .join(city, col("population") === city("c_population"))
+    .select(col("name"), col("country"), col("c_population").as("population"))
+    .show()
+
 }
