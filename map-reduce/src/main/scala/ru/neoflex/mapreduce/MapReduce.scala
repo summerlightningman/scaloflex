a0c0bcfbf584ce093ab690a5d27b80491ef26358
bagulnik <div8@bk.ru>
2020-01-09 13:30:22 +0300

    Поправки в 8 Д/З

--- map-reduce/src/main/scala/ru/neoflex/mapreduce/MapReduce.scala
+++
@@ -1,9 +0,0 @@
-package ru.neoflex.mapreduce
-
-class MapReduce[A, K, B] extends Master with Shuffle with Map with Reader with Reducer with Writer {
-
-  override type ParsedValue = A
-  override type Key = K
-  override type MappedValue = B
-
-}
